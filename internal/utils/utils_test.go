683651fdd0e2bd3ea909f140549e865f406e2d5b
Daniil Shadrin <rabbit72rus@gmail.com>
2021-08-15 13:42:21 +0300

    task-3: add Hobby and funcs to work with

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -4,8 +4,83 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/google/uuid"
+	"github.com/ozonva/ova-hobby-api/pkg/hobby"
 	"github.com/stretchr/testify/require"
 )
+
+func TestHobbiesToMap(t *testing.T) {
+	h1 := hobby.NewHobby("SUP", 0, hobby.Sports)
+	h2 := hobby.NewHobby("Collecting stamps", 1, hobby.Collecting)
+
+	h3 := h2
+	h3.ID = h1.ID
+
+	tests := []struct {
+		inputSlice         []hobby.Hobby
+		expected           map[uuid.UUID]hobby.Hobby
+		expectedErrMessage string
+	}{
+		{[]hobby.Hobby{}, map[uuid.UUID]hobby.Hobby{}, ""},
+		{[]hobby.Hobby{h1}, map[uuid.UUID]hobby.Hobby{h1.ID: h1}, ""},
+		{[]hobby.Hobby{h1, h2}, map[uuid.UUID]hobby.Hobby{h1.ID: h1, h2.ID: h2}, ""},
+
+		{nil, nil, "hobbies slice is not initialized"},
+		{[]hobby.Hobby{h1, h3}, nil, fmt.Sprintf("more than one key {%s} in the resulting map", h1.ID)},
+	}
+
+	for _, testCase := range tests {
+		testName := fmt.Sprintf("%v --> %v", testCase.inputSlice, testCase.expected)
+
+		t.Run(testName, func(t *testing.T) {
+			actual, err := HobbiesToMap(testCase.inputSlice)
+
+			if testCase.expectedErrMessage != "" {
+				require.EqualError(t, err, testCase.expectedErrMessage)
+			}
+			require.Equal(t, testCase.expected, actual, "Maps must be the same")
+		})
+	}
+}
+
+func TestSliceHobbiesIntoBatches(t *testing.T) {
+	h1 := hobby.NewHobby("SUP", 0, hobby.Sports)
+	h2 := hobby.NewHobby("Collecting stamps", 1, hobby.Collecting)
+	h3 := hobby.NewHobby("Cooking", 612, hobby.Domestic)
+	h4 := hobby.NewHobby("Hikking", 98643, hobby.Outdoors)
+
+	tests := []struct {
+		inputSlice         []hobby.Hobby
+		inputBatchSize     int
+		expected           [][]hobby.Hobby
+		expectedErrMessage string
+	}{
+		{[]hobby.Hobby{h1}, 1, [][]hobby.Hobby{{h1}}, ""},
+		{[]hobby.Hobby{h1, h2}, 1, [][]hobby.Hobby{{h1}, {h2}}, ""},
+		{[]hobby.Hobby{h1, h2}, 2, [][]hobby.Hobby{{h1, h2}}, ""},
+		{[]hobby.Hobby{h1, h2, h3, h4}, 2, [][]hobby.Hobby{{h1, h2}, {h3, h4}}, ""},
+		{[]hobby.Hobby{h1, h2, h3, h4}, 3, [][]hobby.Hobby{{h1, h2, h3}, {h4}}, ""},
+
+		{nil, 1, nil, "target slice is not initialized"},
+		{[]hobby.Hobby{}, 1, nil, "target slice must contain at least 1 element, yours has 0"},
+		{[]hobby.Hobby{{}}, 2, nil, "batchSize cannot exceed the len of target, you put 2 - length of the slice is 1"},
+		{[]hobby.Hobby{{}}, 0, nil, "batchSize cannot be less than 1, you put 0"},
+		{[]hobby.Hobby{{}}, -5, nil, "batchSize cannot be less than 1, you put -5"},
+	}
+
+	for _, testCase := range tests {
+		testName := fmt.Sprintf("%v,%v --> %v", testCase.inputSlice, testCase.inputBatchSize, testCase.expected)
+
+		t.Run(testName, func(t *testing.T) {
+			actual, err := SliceHobbiesIntoBatches(testCase.inputSlice, testCase.inputBatchSize)
+
+			if testCase.expectedErrMessage != "" {
+				require.EqualError(t, err, testCase.expectedErrMessage)
+			}
+			require.Equal(t, testCase.expected, actual, "Slices must be the same")
+		})
+	}
+}
 
 func TestSliceIntsIntoBatches(t *testing.T) {
 	tests := []struct {
@@ -18,7 +93,6 @@ func TestSliceIntsIntoBatches(t *testing.T) {
 		{[]int{0, 1}, 2, [][]int{{0, 1}}, ""},
 		{[]int{0, 1, 2, 3}, 2, [][]int{{0, 1}, {2, 3}}, ""},
 		{[]int{0, 1, 2, 3}, 3, [][]int{{0, 1, 2}, {3}}, ""},
-		{[]int{0, 1, 2, 3}, 4, [][]int{{0, 1, 2, 3}}, ""},
 
 		{nil, 1, nil, "target slice is not initialized"},
 		{[]int{}, 1, nil, "target slice must contain at least 1 element, yours has 0"},
