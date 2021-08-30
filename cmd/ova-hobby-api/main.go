4ebd98002b00a6e2e373973b12e054a105f8248c
Daniil Shadrin <rabbit72rus@gmail.com>
2021-08-30 09:48:06 +0300

    task-6: grpc server (#6)

--- cmd/ova-hobby-api/main.go
+++ cmd/ova-hobby-api/main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"time"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+
+	api "github.com/ozonva/ova-hobby-api/internal/app"
+	desc "github.com/ozonva/ova-hobby-api/pkg/github.com/ozonva/ova-hobby-api/pkg/ova-hobby-api"
 )
 
+const (
+	grpcServerEndpoint = "localhost:8082"
+)
+
+func run() error {
+	listen, err := net.Listen("tcp", grpcServerEndpoint)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	desc.RegisterHobbyAPIServer(s, api.NewHobbyAPI())
+
+	log.Println("Server is running")
+	if err := s.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+	return nil
+}
+
 func main() {
-	// example of working with files in an infinite loop
-	for {
-		file, err := os.Open("./README.md")
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		func() {
-			defer file.Close()
-			// some actions with the file can be placed here
-		}()
-		fmt.Println(file.Close())
-		time.Sleep(time.Second * 1)
+	if err := run(); err != nil {
+		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -8,10 +8,15 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.16.0
+	github.com/rs/zerolog v1.24.0
 	github.com/stretchr/testify v1.7.0
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
 	go.uber.org/zap v1.19.0
+	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
 	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
-	golang.org/x/tools v0.1.5 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210828152312-66f60bf46e71 // indirect
+	google.golang.org/grpc v1.40.0
+	google.golang.org/protobuf v1.27.1
 )
